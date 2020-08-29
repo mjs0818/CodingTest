23d5cd37ac276d819a05e13a288b1beb99ec472b
mjs0818 <tjfaudwo09@gmail.com>
2020-08-29 13:54:25 +0900

    K번째 수

---
+++ programmers/Lv1/13.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+  let answer = [];
+  commands.forEach(element => {
+    let arr = array.slice(element[0]-1, element[1])
+    .sort((a,b)=>{return a-b});
+    answer.push(arr[element[2]-1]);      
+  });
+  return answer;
+}
