87157f80e2e810720da10bbf776c2856c16ea4dd
mjs0818 <tjfaudwo09@gmail.com>
2020-08-30 17:45:51 +0900

    핸드폰 번호 가리기

--- programmers/Lv1/31.js
+++ programmers/Lv1/31.js
@@ -1,6 +1,5 @@
 function solution(phone_number) {
     let length = phone_number.length;
     let substring = phone_number.slice(length-4, length);
-    
     return '*'.repeat(length-4) + substring;
 }
