5eaa661f42651cc57d250fd0ba2710aaf339410a
mjs0818 <tjfaudwo09@gmail.com>
2020-08-30 17:06:20 +0900

    정수 제곱근 판별

--- programmers/Lv1/24.js
+++ programmers/Lv1/24.js
@@ -1,3 +1,3 @@
 function solution(n) {
-    return Math.sqrt(n) === parseInt(Math.sqrt(n)) ? Math.pow(n+1,2) : -1;
+    return Math.sqrt(n) === parseInt(Math.sqrt(n)) ? Math.pow(Math.sqrt(n)+1,2) : -1;
 }
