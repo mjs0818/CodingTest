bda255e70f8b269a7f4ede612bbce37522b8894f
mjs0818 <tjfaudwo09@gmail.com>
2020-12-03 22:51:58 +0900

    largestProductOfThree

--- ToyProblem/largestProductOfThree.js
+++ ToyProblem/largestProductOfThree.js
@@ -1,29 +1,7 @@
-const largestProductOfThree = function (array) {
-  // TODO: Your code here!
-  let positive = 0;
-  let negative = 0;
-  array.sort((a, b) => Math.abs(b) - Math.abs(a)).forEach(el => {
-    if(el >= 0){
-      positive++;
-    }else{
-      negative++;
-    }
-  });
-  if(positive === 0){
-    return array.slice(array.length-3).reduce((acc,cur)=>acc*cur);
-  }
-  if(negative === 1){
-    return array.filter(el=>el>0).slice(0,3).reduce((acc,cur)=>acc*cur);
-  }
-  let threeNum = array.slice(0,3);
-  if(threeNum.reduce((acc,cur)=>acc*cur)>=0){
-    return threeNum.reduce((acc,cur)=>acc*cur);
-  }else{
-    let case1 = threeNum.filter(el=>el>0)
-    case1.push(array.slice(3).find(el=>el>0));
-    case1 = case1.reduce((acc,cur)=>acc*cur);
-    let case2 = Math.max(...threeNum) * Math.min(...threeNum) * array.slice(3).find(el=>el<0);
-    return case1 > case2 ? case1 : case2;
-  }
+const largestProductOfThree = function (arr) {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const len = arr.length;
+  const candi1 = sorted[len - 1] * sorted[len - 2] * sorted[len - 3];
+  const candi2 = sorted[len - 1] * sorted[0] * sorted[1];
+  return Math.max(candi1, candi2);
 };
-
