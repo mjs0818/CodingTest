da43e8afbd23a824db6969c8e18994d26a1c9bb1
mjs0818 <tjfaudwo09@gmail.com>
2021-01-04 22:46:17 +0900

    isSubsetOf

--- ToyProblem/isSubsetOf.js
+++ ToyProblem/isSubsetOf.js
@@ -1,9 +1,20 @@
-Array.prototype.isSubsetOf = function(array) {
-    let isSubset = true;
-    this.forEach(element => {
-        if(!array.includes(element)){
-            isSubset = false;
-        }
-    });
-    return isSubset;
-}
+const isSubsetOf = function (base, sample) {
+  let baseIndex = 0;
+  base.sort((a, b) => a - b);
+  sample.sort((a, b) => a - b);
+  const findItem = (item, arr, from) => {
+    for (let i = from; i < arr.length; i++) {
+      if (item === arr[i]) return i;
+      else if (item < arr[i]) return -1;
+    }
+    return -1;
+  };
+
+  for (let i = 0; i < sample.length; i++) {
+    baseIndex = findItem(sample[i], base, baseIndex);
+    if (baseIndex === -1) {
+      return false;
+    }
+  }
+  return true;
+};
