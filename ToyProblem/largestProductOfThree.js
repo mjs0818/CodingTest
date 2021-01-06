71199b847b9b8aaa81131786cf07036a75b78f88
mjs0818 <tjfaudwo09@gmail.com>
2021-01-06 23:40:57 +0900

    largestProductOfThree

--- ToyProblem/largestProductOfThree.js
+++ ToyProblem/largestProductOfThree.js
@@ -1,7 +1,8 @@
 const largestProductOfThree = function (arr) {
-  const sorted = arr.slice().sort((a, b) => a - b);
-  const len = arr.length;
-  const candi1 = sorted[len - 1] * sorted[len - 2] * sorted[len - 3];
-  const candi2 = sorted[len - 1] * sorted[0] * sorted[1];
-  return Math.max(candi1, candi2);
+  // TODO: 여기에 코드를 작성합니다.
+  let sorted = arr.slice().sort((a, b) => a - b);
+  let length = arr.length;
+  let case1 = sorted[0] * sorted[1] * sorted[length - 1];
+  let case2 = sorted[length - 1] * sorted[length - 2] * sorted[length - 3];
+  return Math.max(case1, case2);
 };
