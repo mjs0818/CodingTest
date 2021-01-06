a040485d25305e7c95ab5fad7c33cf011a94e3a5
mjs0818 <tjfaudwo09@gmail.com>
2021-01-06 23:40:00 +0900

    power

--- ToyProblem/power.js
+++ ToyProblem/power.js
@@ -1,18 +1,14 @@
 function power(base, exponent) {
   // todo: 여기에 코드를 작성합니다.
   if (exponent === 0) {
-    return 1
+    return 1;
   }
-
-  if (exponent === 1) {
-    return base
-  }
-
-  let result = power(base, Math.floor(exponent / 2))
-  result = (result * result) % 1000000009
+  let half = parseInt(exponent / 2);
+  let temp = power(base, half);
+  let result = (temp * temp) % 1000000009;
   if (exponent % 2 === 1) {
-    return (result * base) % 1000000009
+    return (result * base) % 1000000009;
   } else {
-    return result
+    return result;
   }
 }
