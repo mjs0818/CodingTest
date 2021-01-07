ee4e234a5af9203aaf9b6f7de8d889d58b7fcd49
mjs0818 <tjfaudwo09@gmail.com>
2021-01-07 16:23:01 +0900

    insertionSort

--- ToyProblem/insertionSort.js
+++ ToyProblem/insertionSort.js
@@ -1,52 +1,19 @@
-var testingTransform = function (array) {
-  var transform = [];
-
-  for (var i = 0; i < array.length; i++)
-    transform.push({
-      value: array[i],
-      i: i
-    });
-
-  return transform;
-};
-
-var insertionSort = function (array, callback) {
-  // TODO: 코드를 여기에 작성합니다.
-  if (!callback) {
-    for (let i = 0; i < array.length; i++) {
-      for (j = i - 1; j >= 0; j--) {
-        if (array[i].value < array[j].value) {
-          let temp = array[j];
-          array[j] = array[i];
-          array[i] = temp;
-          i--;
-        }
-      }
-    }
-  }else{
-    for (let i = 0; i < array.length; i++) {
-      for (j = i - 1; j >= 0; j--) {
-        if (callback(array[i], array[j]) < 0) {
-          let temp = array[j];
-          array[j] = array[i];
-          array[i] = temp;
-          i--;
+const insertionSort = function (arr, callback = (item) => item) {
+  // TODO: 여기에 코드를 작성합니다.
+  let sorted = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (callback(sorted[i - 1]) <= callback(arr[i])) {
+      sorted.push(arr[i]);
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (callback(sorted[j]) >= callback(arr[i])) {
+          let left = sorted.slice(0, j);
+          let right = sorted.slice(j);
+          sorted = left.concat(arr[i], right);
+          break;
         }
       }
     }
   }
-  return array;
+  return sorted;
 };
-
-// let array = [{ value: 1, i: 0 },
-// { value: 2, i: 2 },
-// { value: 43, i: 3 },
-// { value: 21, i: 4 },
-// { value: 100, i: 1 }]
-let array = testingTransform([0,1,2,3,4,5]);
-console.log(insertionSort([0,1,2,3,4,5], function(a,b){
-  var aIsOdd = a & 1;
-  var bIsOdd = b & 1;
-
-  return aIsOdd - bIsOdd;
-}));
