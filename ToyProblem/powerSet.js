625a1844815e7b54ae17ac616156dca102bd00ff
mjs0818 <tjfaudwo09@gmail.com>
2021-01-07 15:37:35 +0900

    powerSet

--- ToyProblem/powerSet.js
+++ ToyProblem/powerSet.js
@@ -1,30 +1,34 @@
 const powerSet = function (str) {
-  // TODO: Your code here!
-  str.split('');
-  let subset = [];
-  let result = [''];
-  for(let i = 0; i < str.length; i++){
-    if(!subset.includes(str[i])){
-      result.push(str[i]);
-      subset.push(str[i]);
-      if(!result.includes(subset.sort().join(''))){
-        result.push(subset.sort().join(''));
-      }
-    }
-    for(let j = 0; j < str.length; j++){
-      if(!subset.includes(str[j])){
-        subset.push(str[j]);
-        let arr = subset.slice();
-        if(!result.includes(arr.sort().join(''))){
-          result.push(arr.sort().join(''))
-        }else{
-          subset.pop();
-        }
-      } 
-    }
-    subset.shift();
-  }
-  return result;
-};
+  // 정렬
+  const sorted = str.split('').sort();
 
-console.log(powerSet('fun'));
+  // 중복 제거
+  const deduplicated = sorted.reduce((acc, item) => {
+    if (acc[acc.length - 1] === item) {
+      return acc;
+    } else {
+      return acc.concat(item);
+    }
+  });
+
+  let subSets = [];
+  const pickOrNot = (idx, subset) => {
+    // base case
+    if (idx === deduplicated.length) {
+      // 마지막 문자까지 검토한 경우
+      subSets.push(subset);
+      return;
+    }
+
+    // recursive case
+    // idx번째 문자가 포함되지 않는 경우
+    pickOrNot(idx + 1, subset);
+
+    // idx번째 문자가 포함되는 경우
+    pickOrNot(idx + 1, subset + deduplicated[idx]);
+  };
+
+  pickOrNot(0, '');
+
+  return subSets.sort();
+};
