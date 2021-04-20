de8a523680afe9a5674cb604deefae182c60cefc
mjs0818 <tjfaudwo09@gmail.com>
2021-04-20 21:13:13 +0900

    프린터

---
+++ programmers/스택 큐/프린터.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+  let answer = 0;
+  while (priorities) {
+    let max = Math.max(...priorities);
+    for (let i = 0; i < priorities.length; i++) {
+      if (priorities[0] === max) {
+        priorities.shift();
+        answer++;
+        location--;
+        break;
+      } else {
+        priorities.push(priorities.shift());
+        location--;
+        if (location < 0) {
+          location = priorities.length - 1;
+        }
+      }
+    }
+    if (location === -1) {
+      break;
+    }
+  }
+  return answer;
+}
